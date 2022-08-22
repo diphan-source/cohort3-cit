c098f6807fcd9169769e07b849dbb2eb93326b69
diphan-source <ahabwediphan@yahoo.com>
2022-08-22 13:03:49 +0300

    fix-bugs

--- display.py
+++ display.py
@@ -5,8 +5,3 @@ str2 = "Is"
 str3 = "James"
 print(f'"{str1}**{str2}**{str3}"')
 
-p, q, r = 10, 20 ,30
-print(p, q, r)
-
-var = "James" * 2  * 3
-print(var)
--- sum.py
+++ sum.py
@@ -3,7 +3,7 @@ Given two integer numbers return their product only if the product is equal to o
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
-if num1 * num2 <= 1000:
+if num1 * num2 >= 1000:
     print(num1 * num2)
 else:
     print(num1 + num2)

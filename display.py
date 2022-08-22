b50c76c2f34f3e288190eaec5561bd62eca7d64f
diphan-source <ahabwediphan@yahoo.com>
2022-08-22 19:30:15 +0300

    refactor display

--- display.py
+++
@@ -1,7 +0,0 @@
-# Display three string “Name”, “Is”, “James” as “Name**Is**James”
-
-str1 = "Name"
-str2 = "Is"
-str3 = "James"
-print(f'"{str1}**{str2}**{str3}"')
-
--- ex2.py
+++
@@ -1,4 +0,0 @@
-# Make a program that solves and shows the summation of 64 + 32.
-x = 64
-y = 32
-print(f"{x} + {y} = {x+y}")

68658e9aeb94088e901c3f8e8d4f0f284d0355c5
diphan-source <ahabwediphan@yahoo.com>
2022-09-06 22:07:54 +0300

    ch qtn

--- projects/weather.py
+++ projects/weather.py
@@ -1,12 +1,5 @@
 
 
-"""
-Weather Program Python Project Using the requests library, create a program that will take a city name as input and return 
-the current weather for that city.The program should also save the city name and the current weather to a file.
-The program should also be able to read the file and print the city name and the current weather. 
-Use this API to get the weather data: https://openweathermap.org/current
-"""
-
 import requests
 import os
 from dotenv import load_dotenv

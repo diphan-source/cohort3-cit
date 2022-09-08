59697e0402e5746e47dd4920344e76c97a5d3006
diphan-source <ahabwediphan@yahoo.com>
2022-09-08 12:44:57 +0300

    refractor

--- project3/quiz.py
+++ project3/quiz.py
@@ -8,47 +8,35 @@ the program should tell the user how many they got correct and what the correct 
 
 """
 
-def multiple_choice(question, answer, options):
-    print(question)
-    correct_ans = 0
-    incorrect_ans = 0
-    for i in range(len(options)):
-        print(options[i])
-    while True:
-        user_answer = input("Enter your answer: ")
-        if user_answer == answer:
-            print("Correct!")
-            correct_ans += 1
-            break
-        else:
-            print("Incorrect!")
-            incorrect_ans += 1
-            break
-    return correct_ans, incorrect_ans
+def get_questions():
+    questions = []
+    with open("questions.txt", "r") as file:
+        for line in file:
+            questions.append(line.strip().split(","))
+    return questions
+
+def get_answers(questions):
+    answers = []
+    for question in questions:
+        print(question[0])
+        for i in range(1, len(question)):
+            print("{}. {}".format(i, question[i]))
+        answer = input("What is your answer? ")
+        answers.append(answer)
+    return answers
+
+def check_answers(questions, answers):
+    score = 0
+    for i in range(len(questions)):
+        if questions[i][int(answers[i])] == questions[i][1]:
+            score =+ 1
+    return score 
 
 def main():
-    correct_ans = 0
-    incorrect_ans = 0
-    question1 = "What is the capital of France?"
-    answer1 = "a"
-    options1 = ["a. Paris", "b. London", "c. Rome", "d. Madrid"]
-    question2 = "What is the capital of Spain?"
-    answer2 = "d"
-    options2 = ["a. Paris", "b. London", "c. Rome", "d. Madrid"]
-    question3 = "What is the capital of Italy?"
-    answer3 = "c"
-    options3 = ["a. Paris", "b. London", "c. Rome", "d. Madrid"]
-    question4 = "What is the capital of Germany?"
-    answer4 = "b"
-    options4 = ["a. Paris", "b. Berlin", "c. Rome", "d. Madrid"]
-    question5 = "What is the capital of England?"
-    answer5 = "b"
-    options5 = ["a. Paris", "b. London", "c. Rome", "d. Madrid"]
-    correct_ans += multiple_choice(question1, answer1, options1)[0] + multiple_choice(question2, answer2, options2)[0] + multiple_choice(question3, answer3, options3)[0] + multiple_choice(question4, answer4, options4)[0] + multiple_choice(question5, answer5, options5)[0]
-    incorrect_ans += multiple_choice(question1, answer1, options1)[1] + multiple_choice(question2, answer2, options2)[1] + multiple_choice(question3, answer3, options3)[1] + multiple_choice(question4, answer4, options4)[1] + multiple_choice(question5, answer5, options5)[1]
-    print("You got", correct_ans, "correct answers and", incorrect_ans, "incorrect answers.")
+    questions = get_questions()
+    answers = get_answers(questions)
+    score = check_answers(questions, answers)
+    print("You got {} correct answers.".format(score))
     
 if __name__ == "__main__":
     main()
-            
-

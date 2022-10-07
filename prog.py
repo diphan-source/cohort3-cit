0070c9b407aff07ec5396758cc349c47c600481e
diphan-source <ahabwediphan@yahoo.com>
2022-10-07 19:00:49 +0300

    assigment

---
+++ prog.py
@@ -0,0 +1,4 @@
+"""Write a program that creates a file named answers.txt
+ and writes all the answers to the questions in the file.
+ The program should then read the file and 
+display the contents on the screen."""

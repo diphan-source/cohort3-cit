2c6ccd2b1f5a213a2f6e866c7cdf1ac84a522977
diphan-source <ahabwediphan@yahoo.com>
2022-10-13 23:45:13 +0300

    ft_module_currency

---
+++ groups/currency_convertor/currency.py
@@ -0,0 +1,36 @@
+
+import requests
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+
+
+class BaseURL:
+    def __init__(self):
+        self.base_url = 'https://api.apilayer.com/fixer/convert?to={}&from={}&amount={}'
+        self.headers = {'apikey': API_KEY}
+        self.symbols_url = 'https://api.apilayer.com/fixer/symbols'
+
+
+class CurrencyConverter(BaseURL):
+    def __init__(self):
+        super().__init__()
+
+    def convert(self, amount, from_currency, to_currency):
+        url = self.base_url.format(to_currency, from_currency, amount)
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            return json.loads(response.text)['result']
+        else:
+            return 'Error: ' + response.text
+
+    def get_symbols(self):
+        response = requests.get(self.symbols_url, headers=self.headers)
+        if response.status_code == 200:
+            return json.loads(response.text)['symbols']
+        else:
+            return 'Error: ' + response.text

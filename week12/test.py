7a23e857ee5f7238a53d6d74ecf70b503012c168
diphan-source <ahabwediphan@yahoo.com>
2022-11-15 11:33:53 +0300

    ch:numpy_frame

---
+++ week12/test.py
@@ -0,0 +1,119 @@
+# val1 ="Peter's sister's name's \"Anna\""
+# val2 ='Peter\'s sister\'s name\'s \"Anna\"'
+# val3 ="Peter's sister's name's "Anna""
+# val4 ='Peter's sister's name's "Anna"'
+
+# print(val1)
+# print(val2)
+
+# i = 250 
+# print(len(str(i)))
+# while len(str(i)) > 72:
+#     i *= 2 
+# else: 
+#     i //= 2 
+    
+# print(i)
+
+
+# n = 0 
+# while n < 4: 
+#     n += 1
+#     print(n , sep=" ")
+
+# x = 0 
+# y = 2 
+# z = len("Python") 
+# x = y > z 
+# print(x)
+
+# Val = 1 
+# Val2 = 0 
+# Val = Val ^ Val2 
+# print(Val)
+# Val2 = Val ^ Val2 
+# print(Val2)
+# Val = Val ^ Val2
+# print(Val)
+
+
+# a = 0
+# b = a** 0 
+# print(b)
+
+# a = 0 
+# b = a ** 0 
+# if b < a + 1: 
+#     c = 1 
+# elif b == 1: 
+#     c = 2 
+# else: c = 3 
+# print(a + b + c)
+
+
+# i = 10 
+# while i > 0 : 
+#     i -= 3 
+#     print("*") 
+#     if i <= 3: 
+#         break 
+# else: 
+#     print("*")
+    
+    
+
+# Example 1 
+# for i in range(1, 4, 2): 
+    # print("*") 
+# Example 2 
+# for i in range(1, 4, 2): 
+    # print("*", end="") 
+# Example 3 
+# for i in range(1, 4, 2): 
+    # print("*", end="**") 
+# Example 4 
+# for i in range(1, 4, 2): 
+    # print("*", end="**") 
+# print("***")
+
+# x = "20" 
+# y = "30" 
+# print(x > y)
+
+# s = "Hello, Python!" 
+
+# print(s[-14:15])
+
+# lst = ["A", "B", "C", 2, 4] 
+# del lst[0:-2] 
+# print(lst)
+
+# lst = [i // i for i in range(0,4)] 
+# sum = 0 
+# for n in lst: sum += n 
+# print(sum)
+
+# lst = [[c for c in range(r)] for r in range(3)] 
+# for x in lst: 
+    # print(x)
+    # for y in x: 
+        # print(y)
+        # if y < 2: 
+            # print('*', end='')
+            
+            
+            
+# lst = [2 ** x for x in range(0, 11)]
+# print(lst)
+
+# s = 'SPAM' 
+# def f(x): 
+#     return s + 'MAPS' 
+# print(f(s))
+
+# try: 
+#     print("Hello") 
+#     raise Exception 
+#     print(1/0) 
+# except Exception as e: 
+#     print(e)
